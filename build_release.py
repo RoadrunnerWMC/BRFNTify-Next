664d9c2abf73c4acbe68aaca3534d327c866247a
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2021-01-11 01:04:34 -0500

    Port build_release.py change from CLIP

--- build_release.py
+++ build_release.py
@@ -28,6 +28,8 @@ SCRIPT_FILE = 'BRFNTify.py'
 DATA_FOLDERS = ['data']
 DATA_FILES = ['readme.md', 'license.txt', 'format.txt']
 
+EXCLUDE_THREADING = True
+
 # macOS only
 AUTO_APP_BUNDLE_NAME = SCRIPT_FILE.split('.')[0] + '.app'
 FINAL_APP_BUNDLE_NAME = FULL_PROJECT_NAME + '.app'
@@ -122,8 +124,11 @@ print('>>')
 # Excludes
 excludes = ['calendar', 'datetime', 'difflib', 'doctest', 'hashlib', 'inspect',
     'locale', 'multiprocessing', 'optpath', 'os2emxpath', 'pdb',
-    'select', 'socket', 'ssl', 'threading', 'unittest',
+    'select', 'socket', 'ssl', 'unittest',
     'FixTk', 'tcl', 'tk', '_tkinter', 'tkinter', 'Tkinter']
+
+if EXCLUDE_THREADING:
+    excludes.append('threading')
 
 if sys.platform == 'nt':
     excludes.append('posixpath')
